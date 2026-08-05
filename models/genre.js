e32ceeb8282abdff91ddab24ff5bef93d4f9e366
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2026-08-05 14:22:19 +0700

    relasi genre dan komik (FK genre)

--- models/genre.js
+++ models/genre.js
@@ -19,4 +19,36 @@ module.exports = (sequelize, DataTypes) => {
         timestamps: true
     });
 
+    module.exports = (sequelize, DataTypes) => {
+    const Genre = sequelize.define('Genre', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nama: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    }, {
+        tableName: 'genre',
+        timestamps: true
+    });
+
+    Genre.associate = (models) => {
+        Genre.belongsToMany(models.Komik, {
+            through: 'komik_genre',
+            foreignKey: 'genre_id',
+            otherKey: 'komik_id',
+            as: 'komik'
+        });
+    };
+    return Genre;
+};
+
 };
